c066b0a1f46fd59f1ba23dcee835c0b04b1ee6ef
richelbilderbeek <richel@richelbilderbeek.nl>
2020-01-22 09:52:57 +0100

    Go Travis

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # pirouette_example_25
-pirouette example 25: the effect of RNG in the alignment for the error distribution
 
-> For a normal RQE 3 run, 6% of 100 runs gives a higher error in the twins, due to bad RNG. Bad RNG can happen in (1) twin tree creation. and (2) alignment creation.
->
-> Measure the influence of the alignment RNG, by using a same twin tree, but 100 different alignment RNG seed.
+Branch   |[![Travis CI logo](pics/TravisCI.png)](https://travis-ci.org)                                                                                                 |[![AppVeyor logo](pics/AppVeyor.png)](https://appveyor.com)                                                                                               
+---------|--------------------------------------------------------------------------------------------------------------------------------------------------------------|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+`master` |[![Build Status](https://travis-ci.org/richelbilderbeek/pirouette_example_25.svg?branch=master)](https://travis-ci.org/richelbilderbeek/pirouette_example_25) |?
+`develop`|[![Build Status](https://travis-ci.org/richelbilderbeek/pirouette_example_25.svg?branch=develop)](https://travis-ci.org/richelbilderbeek/pirouette_example_25)|?
+
+A [pirouette example](https://github.com/richelbilderbeek/pirouette_examples).
+
+## Results
+
+![](example_25_314/errors.png)
+
+![](example_25_315/errors.png)
+
+![](example_25_316/errors.png)
+
+![](example_25_317/errors.png)
+
+![](example_25_318/errors.png)
+
---
+++ example_25.R
@@ -0,0 +1,101 @@
+# From https://github.com/richelbilderbeek/pirouette_article/issues/45 :
+#
+# Measure the influence of the alignment RNG, by using a same twin tree,
+# but 100 different alignment RNG seed.
+
+suppressMessages(library(pirouette))
+suppressMessages(library(ggplot2))
+
+root_folder <- getwd()
+example_no <- 25
+
+for (rng_seed in seq(314, 318)) {
+
+  print(rng_seed)
+
+  example_folder <- file.path(root_folder, paste0("example_", example_no, "_", rng_seed))
+  dir.create(example_folder, showWarnings = FALSE, recursive = TRUE)
+  setwd(example_folder)
+
+  testit::assert(is_beast2_installed())
+  set.seed(314) # Always the same
+  phylogeny <- create_yule_tree(n_taxa = 6, crown_age = 10)
+
+  alignment_params <- create_alignment_params(
+    sim_tral_fun = get_sim_tral_with_std_nsm_fun(
+      mutation_rate = 0.1,
+      site_model = beautier::create_jc69_site_model()
+    ),
+    root_sequence = create_blocked_dna(length = 1000),
+    rng_seed = rng_seed,
+    fasta_filename = "true_alignment.fas"
+  )
+
+  experiment <- create_gen_experiment()
+  experiment$beast2_options$input_filename <- "true_alignment_gen.xml"
+  experiment$beast2_options$output_state_filename <- "true_alignment_gen.xml.state"
+  experiment$inference_model$mcmc$tracelog$filename <- "true_alignment_gen.log"
+  experiment$inference_model$mcmc$treelog$filename <- "true_alignment_gen.trees"
+  experiment$inference_model$mcmc$screenlog$filename <- "true_alignment_gen.csv"
+  experiment$errors_filename <- "true_errors_gen.csv"
+  experiments <- list(experiment)
+
+  # Set the RNG seed
+  for (i in seq_along(experiments)) {
+    experiments[[i]]$beast2_options$rng_seed <- rng_seed
+  }
+
+  # Shorter on Travis
+  if (is_on_travis()) {
+    for (i in seq_along(experiments)) {
+      experiments[[i]]$inference_model$mcmc$chain_length <- 3000
+      experiments[[i]]$inference_model$mcmc$store_every <- 1000
+    }
+  }
+
+  twinning_params <- create_twinning_params(
+    rng_seed_twin_tree = 314, # Always the same
+    sim_twin_tree_fun = get_sim_bd_twin_tree_fun(),
+    rng_seed_twin_alignment = rng_seed,
+    sim_twal_fun = get_sim_twal_same_n_muts_fun(
+      mutation_rate = 0.1,
+      max_n_tries = 1000
+    ),
+    twin_tree_filename = "twin_tree.newick",
+    twin_alignment_filename = "twin_alignment.fas",
+    twin_evidence_filename = "twin_evidence.csv"
+  )
+
+  pir_params <- create_pir_params(
+    alignment_params = alignment_params,
+    experiments = experiments,
+    twinning_params = twinning_params
+  )
+
+  rm_pir_param_files(pir_params)
+
+  errors <- pir_run(
+    phylogeny,
+    pir_params = pir_params
+  )
+
+  utils::write.csv(
+    x = errors,
+    file = file.path(example_folder, "errors.csv"),
+    row.names = FALSE
+  )
+
+  pir_plot(errors) +
+    ggsave(file.path(example_folder, "errors.png"), width = 7, height = 7)
+
+  pir_to_pics(
+    phylogeny = phylogeny,
+    pir_params = pir_params,
+    folder = example_folder
+  )
+
+  pir_to_tables(
+    pir_params = pir_params,
+    folder = example_folder
+  )
+}
